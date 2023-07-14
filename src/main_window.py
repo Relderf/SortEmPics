737747a7b8394774ed58a9b917bd84b5cc2f42d3
Relderf <56208840+Relderf@users.noreply.github.com>
2023-07-14 20:34:46 -0300

    First attempt of a main window interface

--- src/main_window.py
+++ src/main_window.py
@@ -5,22 +5,33 @@ import os
 
 sg.theme('DarkTanBlue')
 HEADER_IMG = os.getcwd() + os.sep + 'img' + os.sep + 'Mempy principal.png'
-
+PAD_TOP = (0,(20,0))
+PAD_TOP_M = (0,(40,0))
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 600
+LAYOUT_WIDTH = WINDOW_WIDTH - 40
 
 def load_layout():
+    
 
-    layout = [[sg.Text('SortEmPics', font=('_', 30), justification='center', pad=(20,0))],
-            [sg.Text('Select the source folder', font=('',10))],
-            [sg.Button('...', font=('Trebuchet MS',14), size=(16,1), key='jugar')],
-            [sg.InputText('C:\\', font=('Trebuchet MS',14), size=(40,1), key='folder', pad=(20,0))],
-            [sg.Text('Select the destination folder for the production', font=('',10))],
-            [sg.Button('...', font=('Trebuchet MS',14), size=(16,1), key='target')],
-            [sg.InputText('C:\\sortedPics', font=('Trebuchet MS',14), size=(40,1), key='folder', pad=(20,0))],
-            [sg.Button('Start Sorting', key='sort', font=('Verdana',12), pad=(20,0))],
-            [sg.Button('Exit', size=(5,1), key='exit', font=('Verdana',12))]
+    layout = [[sg.Text('SortEmPics', font=('_', 30), justification='center')],
+            [sg.Text('Select the source folder', font=('',10), justification='left', 
+                     size=(40, 1), pad=PAD_TOP_M)],
+            [
+                sg.InputText('C:\\', font=('Trebuchet MS',14), size=(30,1), key='folder'),
+                sg.Button('...', font=('Trebuchet MS',10), size=(5,1), key='jugar')
+            ],
+            [sg.Text('Select the destination folder for the production', font=('',10), 
+                     justification='left', size=(40, 1), pad=PAD_TOP)],
+            [
+                sg.InputText('C:\\sortedPics', font=('Trebuchet MS',14), size=(30,1), key='folder'),
+                sg.Button('...', font=('Trebuchet MS',10), size=(5,1), key='target'),
+            ],
+            [sg.Button('Start Sorting', key='sort', font=('Verdana',16), pad=PAD_TOP_M)],
+            [sg.Button('Exit', size=(5,1), key='exit', font=('Verdana',12), pad=PAD_TOP)]
     ]
     return sg.Window('SortEmPics', 
-                     layout, margins=(30,30),
+                     layout, margins=(20,40),
                      element_justification='c').finalize()
 
 
