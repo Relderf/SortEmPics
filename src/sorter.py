585e339af54ecafa11636aba55afabea9fc1a2ef
Relderf <56208840+Relderf@users.noreply.github.com>
2023-07-18 19:41:29 -0300

    Sorted.py detects files metadata (specially Date Taken instead of File Created date)

--- src/sorter.py
+++ src/sorter.py
@@ -1,3 +1,33 @@
+from src.helpers import load_config
+import os
+from datetime import datetime
+from PIL import Image, ExifTags
+import json
+
+
 def sort_pictures():
-    print("LET'S SORT SOME PICTURES!")
-    pass
+    config = load_config()
+    in_folder_path = config['input_folder']
+    source_items = os.listdir(config['input_folder'])
+    for item in source_items:
+        print(get_file_details(in_folder_path + item))
+
+
+def get_file_details(file):
+    image_exif = Image.open(file)._getexif()
+    if image_exif:
+        exif = { ExifTags.TAGS[k]: v for k, v in image_exif.items() if k in ExifTags.TAGS and type(v) is not bytes }
+        date_obj = datetime.strptime(exif['DateTimeOriginal'], '%Y:%m:%d %H:%M:%S')
+        return {
+            'name': os.path.basename(file),
+            'path': os.path.dirname(file) + "/" + os.path.basename(file),
+            'year': date_obj.year,
+            'month': date_obj.month,
+            'day': date_obj.day,
+            'hour': date_obj.hour,
+            'minute': date_obj.minute,
+            'second': date_obj.second
+        }
+    return {}
+        
+    
