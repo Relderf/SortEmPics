622257f2b33a2940214880864bb36c6dc150eb3d
Relderf <56208840+Relderf@users.noreply.github.com>
2023-07-28 16:32:46 -0300

    The sorter now takes into consideration the Month folder naming criteria stored in the local configs

--- src/sorter.py
+++ src/sorter.py
@@ -9,18 +9,17 @@ def sort_pictures():
     config = load_config()
     in_folder_path = config['input_folder']
     source_items = os.listdir(config['input_folder'])
+    if config["single_file_folder"]:
+        quantities = get_quantities(in_folder_path, source_items)
     for item in source_items:
-        path = in_folder_path + "/" + item
-        metadata = get_file_details(path)
+        input_file_path = in_folder_path + item
+        metadata = get_file_details(input_file_path)
         
         output_folder = config['output_folder']
         year_folder = metadata['year']
-        month_folder = cts.MONTHS[metadata['month']]
-        secure_folder(out_path = output_folder,
-                      year = year_folder,
-                      month = month_folder
-                      )
-        os.rename(path, f"{output_folder}/{year_folder}/{month_folder}/{metadata['name']}")
+        month_folder = format_month(config, metadata['month'])
+        secure_folder(output_folder, year_folder, month_folder)
+        move_file(input_file_path, output_folder, year_folder, month_folder, metadata['name'])
 
 
 def get_file_details(file):
@@ -48,6 +47,32 @@ def secure_folder(out_path, year, month):
         os.mkdir(f"{out_path}/{year}/{month}")
 
 
-
-
+def move_file(in_path, out_path, year, month, name):
+    os.rename(in_path, output_filename(out_path, year, month, name))
     
+    
+def output_filename(out_path, year, month, name):
+    return f"{out_path}{year}/{month}/{name}"
+
+
+def get_quantities(in_folder_path, source_items):
+    quantities = {}
+    for item in source_items:
+        metadata = get_file_details(in_folder_path + item)
+        if metadata['year'] in quantities:
+            if metadata['month'] in quantities[metadata['year']]:
+                quantities[metadata['year']][metadata['month']] += 1
+            else:
+                quantities[metadata['year']][metadata['month']] = 1
+        else:
+            quantities[metadata['year']] = { metadata['month']: 1 }
+    return quantities
+    
+def format_month(config, month_number):
+    name_format = config["month_folder_format"]
+    if name_format == "number":
+        return month_number
+    if name_format == "name":
+        return cts.MONTHS[month_number]
+    if name_format == "number_name":
+        return f"{month_number} - {cts.MONTHS[month_number]}"
