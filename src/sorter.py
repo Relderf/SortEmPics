25e9a8e147fd751b28724e242c0d615ec824c41a
Relderf <56208840+Relderf@users.noreply.github.com>
2023-07-31 19:09:29 -0300

    Sorter now takes into consideration the single_file_folder option, taken from configs

--- src/sorter.py
+++ src/sorter.py
@@ -9,8 +9,11 @@ def sort_pictures():
     config = load_config()
     in_folder_path = config['input_folder']
     source_items = os.listdir(config['input_folder'])
-    if config["single_file_folder"]:
+    if not config["single_file_folder"]:
         quantities = get_quantities(in_folder_path, source_items)
+    else:
+        quantities = None
+    
     for item in source_items:
         input_file_path = in_folder_path + item
         metadata = get_file_details(input_file_path)
@@ -18,8 +21,13 @@ def sort_pictures():
         output_folder = config['output_folder']
         year_folder = metadata['year']
         month_folder = format_month(config, metadata['month'])
-        secure_folder(output_folder, year_folder, month_folder)
-        move_file(input_file_path, output_folder, year_folder, month_folder, metadata['name'])
+        day_folder = metadata['day']
+        if not config["single_file_folder"] and quantities[year_folder][metadata['month']][day_folder] < 2:
+            secure_folder(output_folder, year_folder, month_folder)
+            move_file(input_file_path, output_folder, year_folder, month_folder, metadata['name'])
+        else:
+            secure_folder(output_folder, year_folder, month_folder, day_folder)
+            move_file(input_file_path, output_folder, year_folder, month_folder, metadata['name'], day_folder)
 
 
 def get_file_details(file):
@@ -40,19 +48,20 @@ def get_file_details(file):
     return {}
 
 
-def secure_folder(out_path, year, month):
+def secure_folder(out_path, year, month, day=None):
     if not os.path.exists(f"{out_path}/{year}"):
         os.mkdir(f"{out_path}/{year}")
     if not os.path.exists(f"{out_path}/{year}/{month}"):
         os.mkdir(f"{out_path}/{year}/{month}")
+    if day != None and not os.path.exists(f"{out_path}/{year}/{month}/{day}"):
+        os.mkdir(f"{out_path}/{year}/{month}/{day}")
 
 
-def move_file(in_path, out_path, year, month, name):
-    os.rename(in_path, output_filename(out_path, year, month, name))
-    
-    
-def output_filename(out_path, year, month, name):
-    return f"{out_path}{year}/{month}/{name}"
+def move_file(in_path, out_path, year, month, name, day=None):
+    if day != None:
+        os.rename(in_path, f"{out_path}{year}/{month}/{day}/{name}")
+    else:
+        os.rename(in_path, f"{out_path}{year}/{month}/{name}")
 
 
 def get_quantities(in_folder_path, source_items):
@@ -61,13 +70,17 @@ def get_quantities(in_folder_path, source_items):
         metadata = get_file_details(in_folder_path + item)
         if metadata['year'] in quantities:
             if metadata['month'] in quantities[metadata['year']]:
-                quantities[metadata['year']][metadata['month']] += 1
+                if metadata['day'] in quantities[metadata['year']][metadata['month']]:
+                    quantities[metadata['year']][metadata['month']][metadata['day']] += 1
+                else:
+                    quantities[metadata['year']][metadata['month']][metadata['day']] = 1
             else:
-                quantities[metadata['year']][metadata['month']] = 1
+                quantities[metadata['year']][metadata['month']] = { metadata['day']: 1 }
         else:
-            quantities[metadata['year']] = { metadata['month']: 1 }
+            quantities[metadata['year']] = { metadata['month']: {metadata['day']: 1} }
     return quantities
-    
+
+
 def format_month(config, month_number):
     name_format = config["month_folder_format"]
     if name_format == "number":
