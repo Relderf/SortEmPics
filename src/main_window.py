67abb6886ab7ba690551ca93920066176efea9b6
Relderf <56208840+Relderf@users.noreply.github.com>
2023-08-01 21:02:43 -0300

    Improving main window layout

--- src/main_window.py
+++ src/main_window.py
@@ -5,23 +5,28 @@ from src.sorter import sort_pictures
 
 
 sg.theme('DarkTanBlue')
+PAD_Y = (0,(20,20))
 PAD_TOP = (0,(20,0))
 PAD_TOP_M = (0,(40,0))
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 360
+WINDOW_HEIGHT = 400
 LAYOUT_WIDTH = WINDOW_WIDTH - 40
-DARKER_BACK = '#2d2d2d'
+DARKER_BACK = '#1e212b'
 
 
 def load_layout():
     """ Provides the overall layout of the main window """
-    layout = [[sg.Text('SortEmPics', font=('_', 30), justification='center')],
-            [sg.Button('Start Sorting', key='-SORT-', font=('Verdana',16), pad=PAD_TOP_M)],
-            [sg.Button('Config', key='-CONFIG-', font=('Verdana',16), pad=PAD_TOP_M)],
-            [sg.Button('Exit', size=(5,1), key='-EXIT-', font=('Verdana',12), pad=PAD_TOP)]
+    layout = [
+        [sg.Column([
+            [sg.Text('SortEmPics', font=('_', 30), justification='center', pad=PAD_Y)],
+            [sg.Button('Start Sorting', size=(14,1), key='-SORT-', font=('Verdana',16), pad=PAD_TOP)],
+            [sg.Button('Config', size=(14,1), key='-CONFIG-', font=('Verdana',16), pad=PAD_Y)],
+            [sg.Button('Exit', size=(14,1), key='-EXIT-', font=('Verdana',12), pad=PAD_Y)]
+        ], element_justification='center', background_color=DARKER_BACK,
+        )]
     ]
     return sg.Window('SortEmPics', 
-                     layout, size=(WINDOW_WIDTH, WINDOW_HEIGHT), margins=(20,40),
+                     layout, margins=(50,10),
                      element_justification='c').finalize()
 
 
